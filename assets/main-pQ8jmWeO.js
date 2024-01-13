d6e0c414e98bf34fcd7d8cdf0a14cbaac16307f6
Anand raja <meetanandns@gmail.com>
2024-01-13 11:18:38 +0000

    Updates

---
+++ assets/main-pQ8jmWeO.js
@@ -0,0 +1,78 @@
+import{c as t,b as i,h as o,a as l}from"./footer-9uVxrdr5.js";const r=`<main>
+  <section id="product-overview">
+    <h1>Get the freedom you deserve.</h1>
+  </section>
+
+  <section id="plans">
+    <h1 class="section-title">Choose Your Plan</h1>
+    <div class="plan__list">
+      <article class="plan">
+        <h1 class="plan__title">FREE</h1>
+        <h2 class="plan__price">$0/month</h2>
+        <h3>For hobby projects or small teams.</h3>
+        <ul class="plan__features">
+          <li class="plan__feature">1 Workspace</li>
+          <li class="plan__feature">Unlimited Traffic</li>
+          <li class="plan__feature">10GB Storage</li>
+          <li class="plan__feature">Basic Support</li>
+        </ul>
+        <div>
+          <button class="button">CHOOSE PLAN</button>
+        </div>
+      </article>
+
+      <article class="plan plan--highlighted">
+        <h1 class="plan__annotation">RECOMMENDED</h1>
+        <h1 class="plan__title">PLUS</h1>
+        <h2 class="plan__price">$29/month</h2>
+        <h3>For ambitious projects.</h3>
+        <ul class="plan__features">
+          <li class="plan__feature">5 Workspaces</li>
+          <li class="plan__feature">Unlimited Traffic</li>
+          <li class="plan__feature">100GB Storage</li>
+          <li class="plan__feature">Plus Support</li>
+        </ul>
+        <div>
+          <button class="button">CHOOSE PLAN</button>
+        </div>
+      </article>
+
+      <article class="plan">
+        <h1 class="plan__title">PREMIUM</h1>
+        <h2 class="plan__price">$99/month</h2>
+        <h3>Your enterprise solution.</h3>
+        <ul class="plan__features">
+          <li class="plan__feature">10 Workspaces</li>
+          <li class="plan__feature">Unlimited Traffic</li>
+          <li class="plan__feature">Unlimited Storage</li>
+          <li class="plan__feature">Priority Support</li>
+        </ul>
+        <div>
+          <button class="button">CHOOSE PLAN</button>
+        </div>
+      </article>
+    </div>
+  </section>
+
+  <section id="key-features">
+      <h1 class="section-title">Many Good Reasons to Stick Around</h1>
+      <ul class="key-feature__list">
+          <li class="key-feature">
+              <div class="key-feature__image">
+              </div>
+              <p class="key-feature__description">3,857,000 Trusting Customers</p>
+          </li>
+          <li class="key-feature">
+              <div class="key-feature__image">
+              </div>
+              <p class="key-feature__description">99.999% Uptime Guarantee</p>
+          </li>
+          <li class="key-feature">
+              <div class="key-feature__image">
+              </div>
+              <p class="key-feature__description">Lightning Fast CDN</p>
+          </li>
+      </ul>
+  </section>
+</main>
+`,e="/css-uhost/",n=t(l),s=n.querySelector(".main-header__brand");s.href=e;n.querySelector("#pkg-link").href=`${e}pages/packages/`;n.querySelector("#cus-link").href=`${e}pages/customers/`;n.querySelector("#host-link").href=`${e}pages/start-hosting/`;const a=document.createElement("img");a.src=i;a.alt="Your favorite uHost";a.className="brand-img";s.appendChild(a);document.querySelector("#nav").innerHTML=n.innerHTML;document.querySelector("#footer").innerHTML=o;document.querySelector("#app").innerHTML=r;console.log("Domain : ",document.location.origin);console.log("Base : ",e);console.group("1️⃣Navbar before modification 👇");console.log(l);console.groupEnd();console.group("2️⃣Navbar after modification 👇");console.log(n.innerHTML);console.groupEnd();
---
+++ assets/packages-6jW0KMkS.js
@@ -0,0 +1,27 @@
+import{c as s,b as r,h as o,a as c}from"./footer-9uVxrdr5.js";const l=`<main>
+  <section class="package" id="plus">
+    <a href="#">
+      <h1 class="package__title">Our PLUS Plan</h1>
+      <h2 class="package__badge">RECOMMENDED</h2>
+      <h2 class="package__subtitle">The most popular choice of our customers.</h2>
+      <p class="package__info">Benefit from increased storage and faster support to ensure that your mission-critical data and applications
+        are always available!</p>
+    </a>
+  </section>
+  <section class="package" id="free">
+    <a href="#">
+      <h1 class="package__title">Our FREE Plan</h1>
+      <h2 class="package__subtitle">An extremely solid start into our hosting world.</h2>
+      <p class="package__info">Get started immediately at zero cost!</p>
+    </a>
+  </section>
+  <div class="clearfix"></div>
+  <section class="package" id="premium">
+    <a href="#">
+      <h1 class="package__title">Our PREMIUM Plan</h1>
+      <h2 class="package__subtitle">All your enterprise needs. Instant support, guaranteed uptime. </h2>
+      <p class="package__info">The best solution for small to large enterprises. Because hosting shouldn't be in the way!</p>
+    </a>
+  </section>
+</main>
+`,a="/css-uhost/",e=s(c),n=e.querySelector(".main-header__brand");n.href=a;e.querySelector("#pkg-link").href=`${a}pages/packages/`;e.querySelector("#cus-link").href=`${a}pages/customers/`;e.querySelector("#host-link").href=`${a}pages/start-hosting/`;const t=document.createElement("img");t.src=r;t.alt="Your favorite uHost";t.className="brand-img";n.appendChild(t);document.querySelector("#nav").innerHTML=e.innerHTML;document.querySelector("#footer").innerHTML=o;document.querySelector("#app").innerHTML=l;
