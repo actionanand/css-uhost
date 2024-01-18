fcf9457f2b1cd5146466645650a2e4fc75cdca0f
Anand raja <meetanandns@gmail.com>
2024-01-18 20:29:03 +0000

    hosting page

---
+++ pages/start-hosting/start-hosting.js
@@ -0,0 +1,61 @@
+import { convertStringToHTML } from '../../shared/js/utils';
+
+import './start-hosting.css';
+import '../../shared/css/shared.css';
+
+import brandIconImgUrl from '../../shared/images/uhost-icon.png';
+
+import htmlStrNav from '../../shared/views/header.html?raw';
+import htmlStrFooter from '../../shared/views/footer.html?raw';
+import htmlStrHome from './start-hosting.html?raw';
+
+
+const BASE_URL = import.meta.env.BASE_URL;
+
+const navHtml = convertStringToHTML(htmlStrNav);
+// const homeHtml = convertStringToHTML(htmlStrHome);
+
+const brandHtml = navHtml.querySelector('.main-header__brand');
+brandHtml.href = BASE_URL;
+
+// ^ Replacing `navigation urls`
+navHtml.querySelector('#pkg-link').href = `${BASE_URL}pages/packages/`;
+navHtml.querySelector('#cus-link').href = `${BASE_URL}pages/customers/`;
+navHtml.querySelector('#host-link').href = `${BASE_URL}pages/start-hosting/`;
+navHtml.querySelector('#pkg-link-mobile').href = `${BASE_URL}pages/packages/`;
+navHtml.querySelector('#cus-link-mobile').href = `${BASE_URL}pages/customers/`;
+navHtml.querySelector('#host-link-mobile').href = `${BASE_URL}pages/start-hosting/`;
+
+
+
+// * Creating `img` tag
+const imgHtml = document.createElement('img');
+imgHtml.src = brandIconImgUrl;
+imgHtml.alt = 'Your favorite uHost';
+imgHtml.className = 'brand-img';
+
+// ? appending `img` tag to `brandHtml`
+brandHtml.appendChild(imgHtml);
+
+
+// ~ inserting modified `html` snip into placeholder
+document.querySelector('#nav').innerHTML = navHtml.innerHTML;
+document.querySelector('#app').innerHTML = htmlStrHome;
+
+document.querySelector('#footer').innerHTML = htmlStrFooter;
+
+// // Mobile nav elements
+const toggleBtn = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+const backdrop = document.querySelector('.backdrop');
+
+// & triggering mobile nav
+toggleBtn.addEventListener('click', () => {
+  backdrop.classList.add('open');
+  mobileNav.classList.add('open');
+});
+
+backdrop.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+  backdrop.classList.remove('open');
+});
